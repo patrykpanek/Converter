56ccbac3077bb1b025aa0ba0d7ed043f0229ac76
Patryk Panek <patryyk221@gmail.com>
2021-12-14 17:25:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,50 @@ let celcius;
 
 // T(°F) = T(°C) × 1.8 + 32
 // T(°C) = (T(°F) - 32) / 1.8
+
+const changeTitle = () => {
+  if (one.textContent === '°C') {
+    one.textContent = '°F';
+    two.textContent = '°C';
+    converter.value = '';
+    result.textContent = '';
+  } else {
+    one.textContent = '°C';
+    two.textContent = '°F';
+    converter.value = '';
+    result.textContent = '';
+  }
+};
+const fahrToCelc = () => {
+  celcius = (converter.value - 32) / 1.8;
+  result.textContent = `${celcius.toFixed(1)} °C`;
+  converter.value = '';
+};
+
+const celctoFahr = () => {
+  fahrenheit = converter.value * 1.8 + 32;
+  result.textContent = `${fahrenheit.toFixed(1)} °F`;
+  converter.value = '';
+};
+
+const conversion = () => {
+  if (converter.value !== '') {
+    if (one.textContent === '°C') {
+      celctoFahr();
+    } else {
+      fahrToCelc();
+    }
+  } else {
+    result.textContent = 'Enter number...';
+    result.style.color = 'red';
+  }
+};
+
+const reset = () => {
+  result.textContent = '';
+  converter.value = '';
+};
+
+changeBtn.addEventListener('click', changeTitle);
+convBtn.addEventListener('click', conversion);
+resetBtn.addEventListener('click', reset);
